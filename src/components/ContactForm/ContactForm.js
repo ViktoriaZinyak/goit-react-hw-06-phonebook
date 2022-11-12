e342f14c10d3ce42b89147fa33662958afe4dabd
ViktoriyaZinyak <viktoriazinyak@gmail.com>
2022-11-12 17:16:13 +0200

    without persist

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,51 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/contactsSlice';
+import { getContacts } from 'redux/selectors';
+import { Form } from './ContactForm.styled';
+
+const ContactForm = () => {
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    contacts.find(contact => contact.name === form.elements.name.value)
+      ? alert(`${form.elements.name.value} is already in contacts`)
+      : dispatch(
+          addContact({
+            name: form.elements.name.value,
+            number: form.elements.number.value,
+          })
+        );
+    form.reset();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        ></input>
+      </label>
+      <button type="submit">Add contact</button>
+    </Form>
+  );
+};
+
+export default ContactForm;
---
+++ src/redux/store.js
@@ -0,0 +1,46 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { contactsReducer } from './contactsSlice';
+// import { filterReducer } from './filterSlice';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedContactsReducer,
+//     filter: filterReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
